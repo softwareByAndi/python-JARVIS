607ead6fab39fcd4fa11ad3b9af4d9e7eec1058d
softwarebyandi <softwarebyandi@gmail.com>
2023-10-28 11:34:19 -0600

    copy audiobook logic to tts_multithread
    after being generalized, the audiobook logic is better,
    and it removes some hard coded temp filenames

--- src/tts_multithread.py
+++ src/tts_multithread.py
@@ -14,20 +14,21 @@ audio_queue = queue.Queue()
 timestamp_str = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
 print(f"Custom formatted timestamp: {timestamp_str}")
 
-def audio_producer(text_content):
+def audio_producer(text_chunks, filepath, filename):
     audio_files = []
-    temp_path = f"./tts_temp/{timestamp_str}"
+    temp_path = f"{filepath}{filename}/temp/{timestamp_str}"
     os.makedirs(temp_path, exist_ok=True)
     
-    for i, paragraph in enumerate(text_content.split("\n\n")):
+    for i, paragraph in enumerate(text_chunks):
         audio = el.text_to_speech(paragraph)
         audio_queue.put(audio)
         audio_files.append(audio)
-        ma.export_to_mp3(audio, f"{temp_path}/edu_lecture_paragraph_{i}.mp3")
+        # save chunks in case of critical error before files can be merged
+        ma.export_to_mp3(audio, f"{temp_path}/{i}.mp3")
     
     # merge and save audio files now.
     concatenated_audio = ma.merge_audio(audio_files)
-    ma.save(concatenated_audio, f"{args.outfile}.mp3")
+    ma.save(concatenated_audio, f"{filepath}{filename}.mp3")
 
 def audio_consumer():
     while True:
@@ -46,19 +47,46 @@ def audio_consumer():
 
 if (__name__ == "__main__"):
     parser = argparse.ArgumentParser(description="Process a filename.")
-    parser.add_argument("infile", type=str, help="The name of the file to process")
-    parser.add_argument("outfile", type=str, help="The name of the file to process")
+    parser.add_argument("file", type=str, help="The name of the file to process")
 
     args = parser.parse_args()
+    
+    if (args.file == None):
+        print("No filename provided.")
+        exit(1)
+    if (args.file.split('.')[-1] != 'txt'):
+        print("File must be a .txt file.")
+        exit(1)
+    
+    _filepath_parts = args.file.split('/')[:-1]
+    _filepath_string = '/'.join(_filepath_parts)
+    filepath = f"./{_filepath_string}/" # note postfixed /
+    filename = args.file.split('/')[-1].replace('.txt', '')
 
-    with open(args.infile, 'r') as file:
+    with open(f"{filename}.txt", 'r') as file:
         text_content = file.read()
 
-
-
+    # split the text into chunks of 500 characters or less (to lessen delay between input output. api limit is 5k characters)
+    paragraphs = text_content.split("\n\n")
+    chunks = []
+    chunk = ""
+    for paragraph in paragraphs:
+        if len(chunk) + len(paragraph) > 500:
+            chunks.append(chunk)
+            chunk = ""
+        if len(paragraph) > 1000:
+            for sentence in paragraph.split('.'):
+                if len(chunk) + len(sentence) > 500:
+                    chunks.append(chunk)
+                    chunk = ""
+                chunk += sentence + "."
+        else:
+            chunk += paragraph + "\n\n"
+    if chunk != "":
+        chunks.append(chunk)
 
     # Start the producer thread
-    producer_thread = threading.Thread(target=audio_producer, args=(text_content,))
+    producer_thread = threading.Thread(target=audio_producer, args=(chunks, filepath, filename,))
     producer_thread.start()
 
     # Start the consumer thread
